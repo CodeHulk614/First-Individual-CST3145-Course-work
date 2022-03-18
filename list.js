9122266c4f21e57177ef82d4b6f607ec3a5b0ad1
Victor Okorafor <82454990+CodeHulk614@users.noreply.github.com>
2022-03-18 23:57:56 +0400

    Add files via upload

---
+++ list.js
@@ -0,0 +1,70 @@
+new Post(
+  1,
+  'French', 
+  500, 
+  'Chris', 
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  2,
+  'English', 
+  350, 
+  'Tim',
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  3,
+  'Sex Education', 
+  270, 
+  'Sam', 
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  4,
+  'Mathematics', 
+  500, 
+  'Rachel',
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  5,
+  'Social Science', 
+  200, 
+  'Chris', 
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  6,
+  'ICT', 
+  400, 
+  'Tim',
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  7,
+  'Chemistry', 
+  270, 
+  'A. A. Ron',
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  8,
+  'Physics', 
+  300, 
+  'Alex', 
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  9,
+  'Phonetics', 
+  140, 
+  'Chuck',
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+),
+new Post(
+  10,
+  'Literature', 
+  277, 
+  'Chuck',
+  'https://images.unsplash.com/photo-1647346797478-557f63a779df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3NXx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=60-fullsize.png'
+)
